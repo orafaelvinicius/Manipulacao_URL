a2d6d6637b9c12a152a297c94ba1aceef76fb8b1
Jumbeba <jumbeba3@gmail.com>
2021-10-25 16:42:30 -0300

    Estruturação de classes.
    Métodos especiais são chamados pelo próprio interpretador Python de acordo com alguma instrução.
    Como implementar métodos especiais em nossas classes para criar comportamentos customizados.
    A diferença entre igualdade (==) e identidade (is).

--- main.py
+++ main.py
@@ -22,3 +22,17 @@ if indice_e_comercial == -1:
 else:
     valor = url_parametros[indice_valor:indice_e_comercial]
 print(valor)
+
+'''
+Para fazer com que sua classe tenha alguns comportamentos específicos, você pode implementar o que chamamos de métodos especiais, como __len__, __str__ e __eq__. Cada um desses métodos é chamado indiretamente pelo próprio interpretador Python quando executamos algumas instruções específicas.
+
+Por exemplo:
+
+objeto = MinhaClasse()
+
+print(objeto)  # => invoca o método objeto.__str__()
+len(objeto)  # => invoca o método objeto.__len__()
+
+outro_objeto = MinhaClasse()
+objeto == outro_objeto  # => invoca o método objeto.__eq__(outro_objeto)
+'''
